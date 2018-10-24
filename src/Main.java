cdf371ec4ed9a60bcf5f7dc53e45ffc4d3d02ded
Gene.A.Wang <448497795@qq.com>
2018-10-24 17:09:33 +0800

    添加JAVA中运用

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import Patterns.StrategyPattern.MallardDuck;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,5 +14,42 @@ public class Main {
         mallardDuck.Fly();
         mallardDuck.LegCount();
         mallardDuck.MackSound();
+
+
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("a");
+        arrayList.add("ab");
+        arrayList.add("abcd");
+        arrayList.add("abc");
+        // 创建类实现接口写法： arrayList.sort(new ComparatorString());
+
+        // 直接new接口写法：
+       /* arrayList.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length()>o2.length()?1:o1.length()<o2.length()?-1:0;
+            }
+        });*/
+
+        //写法三：Lambda表达式
+        arrayList.sort((o1, o2) -> o1.length()>o2.length()?1:o1.length()<o2.length()?-1:0);
+
+        for (String str:arrayList){
+            System.out.println(str);
+        }
+    }
+
+    public static  class  ComparatorString<String> implements  Comparator<String>{
+        @Override
+        public int compare(String s1, String s2) {
+            return s1.toString().length()>s2.toString().length()?1:s1.toString().length()<s2.toString().length()?-1:0;
+        }
+
+        @Override
+        public Comparator reversed() {
+            return null;
+        }
     }
 }
+
+
